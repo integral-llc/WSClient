9c88c1cce85129a8e38af88833937a208c048d0f
Eugen Rata <eugen.rata@gmail.com>
2015-06-17 21:59:23 -0700

    First commit from local

---
+++ WSClient/MainForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WSClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader columnHeader1;
+            System.Windows.Forms.ColumnHeader columnHeader2;
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lvLog = new System.Windows.Forms.ListView();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Timestamp";
+            columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Message";
+            columnHeader2.Width = 500;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(55, 13);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(654, 20);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Location = new System.Drawing.Point(715, 11);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(12, 39);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtIn);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(778, 533);
+            this.splitContainer1.SplitterDistance = 141;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // txtIn
+            // 
+            this.txtIn.AcceptsReturn = true;
+            this.txtIn.AcceptsTab = true;
+            this.txtIn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtIn.Enabled = false;
+            this.txtIn.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIn.Location = new System.Drawing.Point(0, 0);
+            this.txtIn.Multiline = true;
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(778, 141);
+            this.txtIn.TabIndex = 1;
+            this.txtIn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtIn_KeyDown);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lvLog);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.txtOut);
+            this.splitContainer2.Size = new System.Drawing.Size(778, 388);
+            this.splitContainer2.SplitterDistance = 417;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // lvLog
+            // 
+            this.lvLog.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            columnHeader1,
+            columnHeader2});
+            this.lvLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvLog.FullRowSelect = true;
+            this.lvLog.HideSelection = false;
+            this.lvLog.Location = new System.Drawing.Point(0, 0);
+            this.lvLog.MultiSelect = false;
+            this.lvLog.Name = "lvLog";
+            this.lvLog.Size = new System.Drawing.Size(417, 388);
+            this.lvLog.TabIndex = 2;
+            this.lvLog.UseCompatibleStateImageBehavior = false;
+            this.lvLog.View = System.Windows.Forms.View.Details;
+            this.lvLog.VirtualMode = true;
+            this.lvLog.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.lvLog_RetrieveVirtualItem);
+            this.lvLog.SelectedIndexChanged += new System.EventHandler(this.lvLog_SelectedIndexChanged);
+            this.lvLog.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvLog_MouseDoubleClick);
+            // 
+            // txtOut
+            // 
+            this.txtOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOut.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOut.Location = new System.Drawing.Point(0, 0);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOut.Size = new System.Drawing.Size(357, 388);
+            this.txtOut.TabIndex = 3;
+            this.txtOut.WordWrap = false;
+            this.txtOut.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtOut_KeyDown);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 584);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "WebSocket Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListView lvLog;
+        private System.Windows.Forms.TextBox txtOut;
+    }
+}
+
---
+++ WSClient/MainForm.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using WebSocketSharp;
+using ErrorEventArgs = WebSocketSharp.ErrorEventArgs;
+
+namespace WSClient
+{
+    public partial class MainForm : Form
+    {
+        private WebSocket _ws;
+        private bool _connected;
+        private string _settingsFile;
+
+        public class LogEntry
+        {
+            public DateTime Timestamp { get; set; }
+            public Color ForeColor { get; set; }
+            public string Message { get; set; }
+            public bool? IsIn { get; set; }
+        }
+
+        public class Settings
+        {
+            public string LastServer { get; set; }
+            public List<string> CommandsHistory { get; set; } 
+        }
+
+        private readonly List<LogEntry> _logEntries = new List<LogEntry>(); 
+        private Settings _settings;
+        private int _historyIndex;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (_ws != null && _ws.IsAlive)
+                _ws.Close();
+
+            if (_ws != null)
+            {
+                _ws.OnOpen -= WsOnOpen;
+                _ws.OnMessage -= WsOnMessage;
+                _ws.OnClose -= WsOnClose;
+                _ws.OnError -= WsOnError;
+            }
+
+            _connected = false;
+
+            _ws = new WebSocket(txtUrl.Text);
+            _ws.OnOpen += WsOnOpen;
+            _ws.OnMessage += WsOnMessage;
+            _ws.OnClose += WsOnClose;
+            _ws.OnError += WsOnError;
+            _ws.ConnectAsync();
+
+            btnConnect.Enabled = false;
+            txtIn.Enabled = false;
+            AddLog(Color.Black, "Connecting...");
+
+            _settings.LastServer = txtUrl.Text;
+        }
+
+        private void WsOnError(object sender, ErrorEventArgs args)
+        {
+            if (args.Exception != null)
+                AddLog(Color.Red, args.Message + "\r\n" + args.Exception);
+            else
+                AddLog(Color.Red, args.Message);
+        }
+
+        private void WsOnClose(object sender, CloseEventArgs closeEventArgs)
+        {
+            AddLog(Color.Black, "Disconnected");
+            Action a = () =>
+                {
+                    btnConnect.Enabled = true;
+                    btnConnect.Text = "Connect";
+                };
+
+            _connected = false;
+            BeginInvoke(a);
+        }
+
+        private void WsOnMessage(object sender, MessageEventArgs args)
+        {
+            AddLog(Color.Black, args.Data, true);
+        }
+
+        private void WsOnOpen(object sender, EventArgs eventArgs)
+        {
+            _connected = true;
+            AddLog(Color.Green, "Connected");
+            Action a = () =>
+                {
+                    btnConnect.Text = "Disconnect";
+                    btnConnect.Enabled = true;
+                    txtIn.Enabled = true;
+                };
+
+            BeginInvoke(a);
+        }
+
+        private void AddLog(Color color, string message, bool? isIn = null)
+        {
+            Action a = () =>
+                {
+                    LogEntry entry = new LogEntry
+                        {
+                            ForeColor = color,
+                            Message = message,
+                            Timestamp = DateTime.Now,
+                            IsIn = isIn
+                        };
+
+                    _logEntries.Add(entry);
+                    lvLog.VirtualListSize = _logEntries.Count;
+                };
+            if (InvokeRequired)
+                BeginInvoke(a);
+            else
+                a();
+        }
+
+        private void AddLog(Color color, string format, params object[] args)
+        {
+            AddLog(color, string.Format(format, args));    
+        }
+
+        private void lvLog_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            var entry = _logEntries[e.ItemIndex];
+            ListViewItem item = new ListViewItem();
+            item.Text = entry.Timestamp.ToString();
+            string prefix = "";
+            if (entry.IsIn.HasValue)
+                prefix = entry.IsIn == true ? "> " : "< ";
+            item.SubItems.Add(prefix + entry.Message.Substring(0, Math.Min(100, entry.Message.Length)));
+            item.ForeColor = entry.ForeColor;
+            e.Item = item;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _settingsFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "settings.json");
+            try
+            {
+                if (File.Exists(_settingsFile))
+                {
+                    _settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(_settingsFile));
+                }
+            }
+            catch 
+            {}
+
+            if (_settings == null)
+            {
+                _settings = new Settings
+                    {
+                        CommandsHistory = new List<string>()
+                    };
+            }
+
+            txtUrl.Text = _settings.LastServer;
+            if (_settings.CommandsHistory.Count > 0)
+                txtIn.Text = _settings.CommandsHistory[0];
+        }
+
+        private void lvLog_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvLog.SelectedIndices.Count == 0)    
+                return;
+
+            txtOut.Text = _logEntries[lvLog.SelectedIndices[0]].Message;
+        }
+
+        private void txtIn_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!_connected)
+                return;
+
+            if (e.Control)
+            {
+                if (e.KeyCode == Keys.S && txtIn.Text.Length > 0)
+                {
+                    _ws.SendAsync(txtIn.Text, SendCompleted);
+                    AddLog(Color.Black, txtIn.Text, false);
+                    txtIn.Enabled = false;
+                    while (_settings.CommandsHistory.Count > 100)
+                        _settings.CommandsHistory.RemoveAt(_settings.CommandsHistory.Count - 1);
+                    _settings.CommandsHistory.Insert(0, txtIn.Text);
+                    _historyIndex = 0;
+                }
+                else if (e.KeyCode == Keys.Up)
+                {
+                    if (_historyIndex + 1 < _settings.CommandsHistory.Count)
+                    {
+                        _historyIndex++;
+                        txtIn.Text = _settings.CommandsHistory[_historyIndex];
+                    }
+                }
+                else if (e.KeyCode == Keys.Down)
+                {
+                    if (_historyIndex - 1 >= 0)
+                    {
+                        _historyIndex--;
+                        txtIn.Text = _settings.CommandsHistory[_historyIndex];
+                    }
+                }
+            }
+        }
+
+        private void SendCompleted(bool b)
+        {
+            Action a = () =>
+                {
+                    txtIn.Enabled = true;
+                };
+
+            BeginInvoke(a);
+        }
+
+        private void lvLog_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (lvLog.SelectedIndices.Count == 0)
+                return;
+
+            var entry = _logEntries[lvLog.SelectedIndices[0]];
+            if (entry.IsIn == false)
+                txtIn.Text = entry.Message;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText(_settingsFile, JsonConvert.SerializeObject(_settings));
+        }
+
+        private void txtOut_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.J)
+            {
+                if (txtOut.Text.Length > 1)
+                {
+                    try
+                    {
+                        dynamic parsedJson = JsonConvert.DeserializeObject(txtOut.Text);
+                        txtOut.Text = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+                    }
+                    catch { }
+                }
+            }
+        }
+    }
+}
